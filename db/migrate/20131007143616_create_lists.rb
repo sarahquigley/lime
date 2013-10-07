090386b4a40d4a2746c72e1ba0022ab5227e6e3a
Sarah Quigley <sarah@sarahquigley.net>
2013-10-07 11:40:36 -0400

    Add database level checks, db indexes, and a few extra attributes to Lists, Tasks, Users.

--- db/migrate/20131007143616_create_lists.rb
+++ db/migrate/20131007143616_create_lists.rb
@@ -1,13 +1,15 @@
 class CreateLists < ActiveRecord::Migration
   def change
     create_table :lists do |t|
-      t.string :title
-      t.string :label
-      t.boolean :completed
-      t.boolean :archived
       t.integer :user_id
+      t.string :title, null: false
+      t.string :label
+      t.boolean :completed, null: false, default: false
+      t.boolean :archived, null:false, default: false
 
       t.timestamps
     end
+
+    add_index :lists, :user_id
   end
 end
--- db/migrate/20131007145441_create_tasks.rb
+++ db/migrate/20131007145441_create_tasks.rb
@@ -1,16 +1,18 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-      t.string :title
+      t.integer :list_id, null: false
+      t.string :title, null: false
       t.text :description
       t.integer :priority
       t.datetime :due
-      t.boolean :completed
-      t.boolean :archived
-      t.integer :list_position
-      t.integer :list_id
+      t.boolean :completed, null: false, default: false
+      t.boolean :archived, null: false, default: false
+      t.integer :list_position, null: false
 
       t.timestamps
     end
+
+    add_index :tasks, :list_id
   end
 end
