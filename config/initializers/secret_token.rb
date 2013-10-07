e9f4f18064154e42f8c8c93c1b4adec7cc8da241
Sarah Quigley <sarah@sarahquigley.net>
2013-10-07 10:12:33 -0400

    Initialise Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lime::Application.config.secret_token = '7f48b5bc20e0d36fa4b7c6493f154cbb4718455b98d78ffb608c7537a725db77db43b3b28a5a3b998bc525d3f55daba07730176f47715730f16018be5b348d06'
