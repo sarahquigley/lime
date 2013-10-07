ae579ce205c23aa303d8d04ac377ef7c23b6d468
Sarah Quigley <sarah@sarahquigley.net>
2013-10-07 15:30:13 -0400

    Add routes for Lists & Tasks.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Lime::Application.routes.draw do
+
+  # Route for main app, accessed by logged in users
   get "app", to: 'static_pages#app'
 
   devise_for :users do
     root to: 'static_pages#home'
   end
+
+  # List routes
+  resources :lists, except: [:show, :new, :edit]
+
+  # Task routes
+  resources :tasks, only: [:create, :update, :destroy]
 
 end
