16b1c23bebad40a0937eff62ae05ae5c9f5bdba5
Sarah Quigley <sarah@sarahquigley.net>
2013-10-07 11:27:55 -0400

    Add devise (and some relevant configurations).

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,9 @@ Lime::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  # Action mailer default url
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
