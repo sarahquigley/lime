55090ef9e83176fd7b816afeb4a226e480a0f0cd
Sarah Quigley <sarah@sarahquigley.net>
2013-10-11 18:49:32 -0400

    Backbone / Styling: Add task sort menu.

--- app/assets/javascripts/collections/tasks.js
+++ app/assets/javascripts/collections/tasks.js
@@ -1,35 +1,25 @@
 Lime.Collections.Tasks = Backbone.Collection.extend({
 
+  initialize: function(){
+    this.sortAttribute = "title";
+  },
+
   model: Lime.Models.Task,
   url: '/tasks',
 
-  comparator: 'title',
-  //
-  // remaining: function(){
-  //   return this.collectionWhere({completed: false});
-  // },
-  //
-  // completed: function(){
-  //   return this.collectionWhere({completed: true});
-  // },
-  //
-  // unarchived: function(){
-  //   return this.collectionWhere({archived: false});
-  // },
-  //
-  // archived: function(){
-  //   return this.collectionWhere({completed: true});
-  // },
-  //
-  // today: function(){
-  //   return this.collectionWhere({due_to_s: "Today"});
-  // },
-  //
-  // tomorrow: function(){
-  //   return this.collectionWhere({due_to_s: "Tomorrow"});
-  // },
+  comparator: function(model1, model2){
+    model1 = model1.get(this.sortAttribute);
+    model2 = model2.get(this.sortAttribute);
+    return model1 > model2 ?  1 : model1 < model2 ? -1 : 0;
+  },
 
   collectionWhere: function(options){
     return new Lime.Collections.Tasks(this.where(options));
+  },
+
+  sortCollection: function(attribute){
+    this.sortAttribute = attribute;
+    this.sort();
   }
+
 });
--- app/assets/javascripts/views/lists/list_show.js
+++ app/assets/javascripts/views/lists/list_show.js
@@ -1,23 +1,52 @@
 Lime.Views.ListShow = Backbone.View.extend({
 
   initialize: function(){
+    this.collection = this.model.get('tasks');
     this.nestedViews = [];
+    var that = this;
+    var events = ['add', 'change', 'remove'];
+    _(events).each(function(event){
+      that.listenTo(that.model, event, that.render);
+      that.listenTo(that.collection, event, that.render);
+    });
+    that.listenTo(that.collection, 'sort', that.render);
+  },
+
+  events: {
+    "click .app-drop-button": "dropMenu",
+    "click .sort-menu button.sort-tasks": "sort"
   },
 
   el: '<section id="featured-list">',
 
   template: JST['lists/show'],
 
+  menuTemplate: JST['lists/show_menu'],
+
   render: function(){
     this.$el.html(this.template({
-      list: this.model
+      list: this.model,
+      showMenuTemplate: this.menuTemplate
     }));
+
     var tasksIndexView = new Lime.Views.TasksIndex({
-      collection: this.model.get('tasks')
+      collection: this.collection
     });
     this.nestedViews = [tasksIndexView];
     this.$el.append(tasksIndexView.render().$el);
     return this;
+  },
+
+  // Drop Menu (needs click outside collapse)
+
+  dropMenu: function(event){
+    $(event.target).closest('.app-drop-parent').toggleClass('dropped');
+  },
+
+  // Sort
+  sort: function(event){
+    sortAttribute = $(event.target).attr("data-sort");
+    this.collection.sortCollection(sortAttribute);
   }
 
 })
